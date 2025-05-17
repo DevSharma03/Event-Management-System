5535aba552a1569e4865e5ddb5f13f3d2626c878
Devashish Sharma <168680367+DevSharma03@users.noreply.github.com>
2025-05-17 20:59:33 +0530

    Update EventProApplication.java

--- EventPro/src/main/java/com/eventpro/EventProApplication.java
+++ EventPro/src/main/java/com/eventpro/EventProApplication.java
@@ -3,7 +3,7 @@ package com.eventpro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication 
 public class EventProApplication {
     public static void main(String[] args) {
         SpringApplication.run(EventProApplication.class, args);
